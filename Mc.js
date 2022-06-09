c201d1f62e77e2062c1a7cbbe35f8dbec4e5ff57
jishadaj <ajjishad0@gmail.com>
2022-06-09 17:44:34 +0530

    Mcdonalds

---
+++ Mc.js
@@ -0,0 +1,15 @@
+
+let modalBox=document.getElementById('modalBox')
+
+modalBox.style.display='none'
+
+function openModal(){
+    modalBox.style.display='block'
+   
+}
+// function openModal(modalBox){
+//     $(modalBox).modal('show');
+// }
+function closeModal(){
+    modalBox.style.display='none'
+}
